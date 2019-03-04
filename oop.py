dc2fc8a5951679603b67465f9e557f64408eba3b
z-vadim <z_vadim@outlook.com>
2019-03-04 17:35:43 +0200

    change struct

--- oop.py
+++ oop.py
@@ -1,42 +1,55 @@
 class Employee:
     employee_id = 0
     list_of_managers = []
-    emp_position = {'developer', 'designer', 'manager'}
-    team_list = []
+    POSITIONS = {'developer', 'designer', 'manager'}
+    team_list = {}
 
-    def __init__(self, first_name, second_name, salary, experience, eff_coefficient, is_manager, position):
+    def __init__(self, first_name, second_name, salary, experience, team_id):
         self.first_name = first_name
         self.second_name = second_name
         self.salary = salary
         self.experience = experience
-        self.eff_coefficient = eff_coefficient
-        self.is_manager = is_manager
-        self.position = position
         self.team_id = team_id
-
         Employee.employee_id += 1
 
     def getSalary(self):
-
         if self.experience > 2:
-            self.salary + 200
-        elif self.experience > 5:
+            self.salary += 200
+        if self.experience > 5:
             self.salary = (self.salary*1.2)+500
+        return self.salary
 
-        if self.position == "designer":
-            self.salary = self.salary * self.eff_coefficient
-        if self.position == "manager":
-            if len(self.team_list) > 5:
-                self.salary + 200
-            if len(self.team_list) > 10:
-                self.salary + 300
+    def __str__(self):
+        return "%s %s %s %s" % (self.first_name, self.second_name, self.salary, self.experience)
+
+
+class Designer(Employee):
+    def __init__(self, first_name, second_name, salary, experience, team_id, eff_coefficient):
+        super().__init__(first_name, second_name, salary, experience, team_id)
+        self.eff_coefficient = eff_coefficient
+
+    def getSalary(self):
+        return self.salary * self.eff_coefficient
+
+
+class Manager(Employee):
+    def __init__(self, first_name, second_name, salary, experience, team_id):
+        super().__init__(first_name, second_name, salary, experience, team_id)
+
+    def getSalary(self):
+        if len(self.team_list) > 5:
+            self.salary + 200
+        if len(self.team_list) > 10:
+            self.salary + 300
             dev_count = 0
-            for member in self.team_list:
-                if member == 'developer':
-                    dev_count += 1
+        for member in self.team_list:
+            if member == 'developer':
+                dev_count += 1
             if dev_count > len(self.team_list/2):
                 self.salary = self.salary *1.1
         return self.salary
 
-    def represent(self):
-        return "%s %s: manager: %s" %(self.first_name, self.second_name, self.salary)
+
+d=Designer('Ivan','Ivanov', 100, 3, '3', 4)
+print(d)
+print(d.getSalary())
